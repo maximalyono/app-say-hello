03ab9f154e590cded085e54dc5df7e7182776e54
maximalyono <maximalyono@gmail.com>
2021-07-29 10:01:04 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/maximalyono/app-say-hello
+
+go 1.16
+
+require github.com/maximalyono/go-say-hello/v2 v2.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+    "fmt"
+    "github.com/maximalyono/go-say-hello/v2"
+)
+
+func main() {
+    fmt.Println(go_say_hello.SayHello("yuhu"))
+}
